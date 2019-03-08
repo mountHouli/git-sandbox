1ffe656b51eb730c93c51f88f740615ae6681747
Aaron Houlihan <aaron@amida.com>
2019-03-08 12:30:27 -0500

    Add app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const app = express()
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} -----------------------------`)
+  next()
+})
+
+app.get('/master1', (req, res) => {
+  res.status(200).send('master1')
+})
+
+const port = 1337
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error('Error starting server:', err)
+    process.exit(1)
+  }
+  console.log(`App listening on port ${port}`)
+})
